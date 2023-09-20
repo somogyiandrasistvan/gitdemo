b93c0ee884b9a62df852d1a9ce135b87f3921b93
Somogyi András István (SZF_2022_2024) <somogyi.andras.istvan@diak.szamalk-szalezi.hu>
2023-09-20 12:50:32 +0200

    ciklus

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -3,7 +3,9 @@ package gitdemo;
 public class GitDemo {
 
     public static void main(String[] args) {
-        System.out.println("");
+        for (int i = 0; i < 10; i++) {
+            
+        }
     }
     
 }
